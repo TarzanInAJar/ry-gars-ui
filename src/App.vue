6ace4755e7dabf1572b2687000a970aadb750568
TarzanInAJar <rybread@gmail.com>
2020-09-11 12:18:04 -0500

    WIP - playing around with a layout, added a fake search bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,105 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+    <v-navigation-drawer
+        v-model="drawer"
+        app
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="{name: 'Search', query: { text: 'Arturo Fuente'}}">
+          <v-list-item-action>
+            <v-icon>mdi-magnify</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Search for Arturo Fuente</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+        app
+        color="primary"
+        dark
+    >
+      <div style="width: 100%" class="d-flex justify-center align-center">
+        <div style="position: absolute; left:0px;" class="d-flex align-center">
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-toolbar-title class="d-none d-sm-flex">Ry-Gars</v-toolbar-title>
+        </div>
+        <div>
+          <v-autocomplete
+              v-model="text"
+              :items="items"
+              :loading="isLoading"
+              :search-input.sync="search"
+              hide-no-data
+              hide-selected
+              placeholder="Search for a Gar!"
+              single-line
+              hide-details
+              rounded
+              dense
+              solo-inverted
+          >
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+          </v-autocomplete>
+        </div>
+        <div style="position: absolute; right: 10px;" class="d-flex align-center">
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+        </div>
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    drawer: false,
+    isLoading: false,
+    search: null,
+    text: null,
+    items: []
   }),
+  watch: {
+    search(val) {
+      if (val && val !== this.text) {
+        this.searchAPI(val)
+      }
+    }
+  },
+  methods: {
+    searchAPI() {
+      this.isLoading = true;
+      // TODO replace with real api
+      setTimeout(() => {
+        this.items = ['change', 'me']
+        this.isLoading = false
+      }, 500)
+    }
+  }
 };
 </script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
-    TODO
+    Handle search for {{query}}
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  props: ['query']
 }
 </script>
